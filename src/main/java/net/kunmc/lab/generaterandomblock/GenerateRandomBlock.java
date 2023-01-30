2f4f0672000e1d2d215fadde494f7e4e257b67fe
yanayana8721 <yanayana8721@gmail.com>
2023-01-31 05:15:45 +0900

    完成

--- src/main/java/net/kunmc/lab/generaterandomblock/GenerateRandomBlock.java
+++ src/main/java/net/kunmc/lab/generaterandomblock/GenerateRandomBlock.java
@@ -11,49 +11,64 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.RayTraceResult;
 
 import java.util.*;
 
 public final class GenerateRandomBlock extends JavaPlugin implements Listener {
     public Map<Player, Block> playerBlockMap = new HashMap<>();
-    public Map<Player, Location> playerLocationMap = new HashMap<>();
-    public List<Material> blockList ;
+    public List<Material> blockList = new ArrayList<>();
 
     @Override
     public void onEnable() {
-
         //ブロックリストの作成
         Material[] materials = Material.values();
         for (Material material : materials) {
             if (material.isBlock() && !material.isAir()) {
-                blockList.add(material);
+                if(material != Material.AIR && material != Material.CAVE_AIR && material != Material.VOID_AIR){
+                    blockList.add(material);
+                }
             }
         }
-
         getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    public double solidDistance(Location l1,Location l2){
+        double p1x = l1.getX() , p1y = l1.getY() , p1z = l1.getZ();
+        double p2x = l2.getX() , p2y = l2.getY() , p2z = l2.getZ();
+        return Math.sqrt(Math.pow(p2x-p1x,2)+Math.pow(p2y-p1y,2)+Math.pow(p2z-p1z,2));
     }
 
     @EventHandler
     public void onShowBlock(PlayerMoveEvent e){
-        Player player = e.getPlayer();
         try{
-            Block block = player.rayTraceBlocks(16).getHitBlock();
-            Block changeBlock = block.getRelative(player.rayTraceBlocks(16).getHitBlockFace());
+            Player p = e.getPlayer();
 
-            Material material;
-            if(block.getType().isAir()) return;
-            if(playerLocationMap.containsKey(player)){
-                if(playerLocationMap.get(player)==block.getLocation()) return;
-                material = playerBlockMap.get(player).getType();
-                changeBlock.setType(material);
-            }else{
+            RayTraceResult result = p.rayTraceBlocks(16);
+
+            Block hitBlock = result.getHitBlock();
+            Block changeBlock = hitBlock.getRelative(result.getHitBlockFace());
+
+            if(hitBlock.getType().isAir()) return;
+            if(!changeBlock.getType().isAir()) return;
+
+            if(solidDistance(p.getLocation(),changeBlock.getLocation()) < 1) return;
+
+            if(!playerBlockMap.containsKey(p)){
                 Random r = new Random();
-                material = blockList.get(r.nextInt(blockList.size()));
-                changeBlock.setType(material);
-                playerBlockMap.put(player,changeBlock);
-                playerLocationMap.put(player,changeBlock.getLocation());
+                Material m = blockList.get(r.nextInt(blockList.size()));
+                changeBlock.setType(m);
+                playerBlockMap.put(p,changeBlock);
+                p.sendMessage(m.name());
+            }else{
+                if(changeBlock.getLocation() == playerBlockMap.get(p).getLocation()) return;
+                Material m = playerBlockMap.get(p).getType();
+                changeBlock.setType(m);
+                p.sendMessage(m.name());
             }
-        } catch(NullPointerException exception){
+
+
+        } catch(NullPointerException ignored){
         }
     }
 
